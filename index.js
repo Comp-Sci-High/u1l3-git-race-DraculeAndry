72c5a1bdc1676ef98184971a03cc5bd24c2afd80
Andry <andry.hernandez26@compscihigh.org>
2024-09-18 15:14:41 +0000

    Printing my name

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-
+console.log("Andry")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Blue-bagged Sun Chips")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+console.log("Kordhell")
 // Stage and commit this change with an appropriate message
 // Go to more.js
