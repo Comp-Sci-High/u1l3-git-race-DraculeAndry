12e07a4f5e2f6f9898e38a9aace86f4301bbb653
Andry <andry.hernandez26@compscihigh.org>
2024-09-18 15:15:09 +0000

    Printing and commiting more.js

--- more.js
+++ more.js
@@ -1,9 +1,9 @@
 // Task 4
 // Print your favorite food space
-
+console.log("Home")
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+console.log("BLT with Egg and Mozzarella Cheese on a hero")
 // Stage and commit this change with an appropriate message
 // Go to README.md
